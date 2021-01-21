ad6653ecb4abcf814761463d79dcaa57525805a6
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2021-01-21 23:46:52 +0600

    Add files via upload

---
+++ JavaScript/Stack.js
@@ -0,0 +1,40 @@
+class Stack {
+    constructor() {
+        this.data = [];
+        this.top = 0;
+    }
+    push(element) {
+        this.data[this.top] = element;
+        this.top = this.top + 1;
+    }
+    length() {
+        return this.top;
+    }
+    peek() {
+        return this.data[this.top - 1];
+    }
+    isEmpty() {
+        return this.top === 0;
+    }
+    pop() {
+        if (this.isEmpty() === false) {
+            this.top = this.top - 1;
+            return this.data.pop(); // removes the last element
+        }
+    }
+    print() {
+        var top = this.top - 1; // because top points to index where new    element to be inserted
+        while (top >= 0) { // print upto 0th index
+            console.log(this.data[top]);
+            top--;
+        }
+    }
+}
+
+var newstack = new Stack
+
+newstack.push(1)
+newstack.push(2)
+newstack.pop()
+
+console.log(newstack)
